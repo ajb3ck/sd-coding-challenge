5a8f0484639e3b0fb8271c8fcd8d259ccd0e7307
AJ Beck <aj.beck@healthstream.com>
2019-12-11 07:47:25 -0600

    [minor] (initial): Add additional unit tests

--- js-lambda-api-gateway/lambdas/launchpads/test/index.test.js
+++ js-lambda-api-gateway/lambdas/launchpads/test/index.test.js
@@ -1,5 +1,33 @@
+import nock from 'nock';
+import { launchPadsResp } from './nockRespData';
+import { defaultEvent } from './sampleEvents';
+
+import { handler } from '../src/index';
+
+const httpMocks = {};
+
+beforeAll(() => {
+  httpMocks.GETLaunchpads = nock('https://api.spacexdata.com')
+    .get('/v2/launchpads')
+    .delayBody(200)
+    .reply(200, launchPadsResp);
+});
+
+afterAll(() => {
+  nock.cleanAll();
+});
+
 describe('Handler Tests', () => {
-  test('Happy Path', () => {
-    expect(true).toBe(true);
+  test('Happy Path', async () => {
+    const event = defaultEvent;
+    const context = {};
+
+    const expectedBody = expect.stringContaining('"Launchpad Id":"vafb_slc_4e"');
+    const expected = {
+      statusCode: 200,
+      body: expectedBody,
+    };
+
+    await expect(handler(event, context)).resolves.toMatchObject(expected);
   });
 });
--- js-lambda-api-gateway/lambdas/launchpads/test/modules/createResponse.test.js
+++ js-lambda-api-gateway/lambdas/launchpads/test/modules/createResponse.test.js
@@ -1,5 +1,27 @@
+import { CreateResponse } from '../../src/modules/CreateResponse';
+import { launchPadsResp } from '../nockRespData';
+
 describe('Handler Tests', () => {
   test('Happy Path', () => {
-    expect(true).toBe(true);
+    const params = { launchPadData: launchPadsResp };
+    const expectedBody = expect.stringContaining('"Launchpad Id":"vafb_slc_4e",');
+
+    const expected = {
+      statusCode: 200,
+      body: expectedBody,
+    };
+
+    expect(CreateResponse(params)).toMatchObject(expected);
+  });
+  test('Happy Sad Path: Invalid Option privded for match param', () => {
+    const params = { launchPadData: launchPadsResp, match: 'notvalid' };
+    const expectedBody = expect.stringContaining('"type":"VALIDATION_ERROR"');
+
+    const expected = {
+      statusCode: 400,
+      body: expectedBody,
+    };
+
+    expect(CreateResponse(params)).toMatchObject(expected);
   });
 });
